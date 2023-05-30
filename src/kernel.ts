6e3a1c2209e67c257f3b52d04ec2b24e6bd91ad9
tianhe li <mr.ying.lee@gmail.com>
2023-05-30 22:00:19 +0800

    modified:   src/kernel.ts

--- src/kernel.ts
+++ src/kernel.ts
@@ -55,7 +55,8 @@ export class ChatKernel extends BaseKernel {
   ): Promise<KernelMessage.IExecuteReplyMsg['content']> {
     if (content.code.trim().toLowerCase().startsWith('key=')) {
       configuration.apiKey = content.code.trim().slice('key='.length);
-
+      delete configuration.baseOptions.headers['User-Agent'];
+      
       myOpenAI = new OpenAIApi(configuration);
 
       this.publishExecuteResult({
