1c2996316d79add846dce4600e3e0e7d96f99764
tianhe li <mr.ying.lee@gmail.com>
2023-06-02 16:49:08 +0800

    deleted:    types/handlebars1.ts 	src/types/

--- types/handlebars1.ts
+++
@@ -1,6 +0,0 @@
-// https://github.com/handlebars-lang/handlebars.js/pull/1862
-declare module 'handlebars/lib/handlebars' {
-    // Re-export the types from the existing handlebars module
-    import Handlebars = require('handlebars');
-    export default Handlebars;
-  }
