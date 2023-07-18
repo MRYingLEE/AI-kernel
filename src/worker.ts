89baf88613be8f75f9d0784cdf368e7e3eabf31b
Ying LI <Mr.Ying.LEE@gmail.com>
2023-07-18 09:16:19 +0800

    Moved core logic to Workd

--- src/worker.ts
+++ src/worker.ts
@@ -1,12 +1,12 @@
 import { IAIWorkerKernel } from './common/tokens';
-import { extractPersonAndMessage } from './worker_AI/chatSyntax';
+// import { extractPersonAndMessage } from './worker_AI/chatSyntax';
 
-import { backOff } from 'exponential-backoff';
-import { OpenAIDriver } from './worker_AI/driver_azure';
-// import { ChatMessage } from 'openai';
-import { ChatMessage } from '@azure/openai';
+// import { backOff } from 'exponential-backoff';
+// import { OpenAIDriver } from './worker_AI/driver_azure';
+// // import { ChatMessage } from 'openai';
+// import { ChatMessage } from '@azure/openai';
 
-import { promptTemplate } from './worker_AI/promptTemplate';
+// import { promptTemplate } from './worker_AI/promptTemplate';
 import { MyConsole } from './worker_AI/controlMode_Worker';
 
 import {
@@ -109,25 +109,25 @@ export class AIRemoteKernel {
       };
     }
 
-    function publish_execute_error(result: string, executionCount: number) {
-      const bundle = {
-        data: {
-          'text/plain': result
-        },
-        metadata: {},
-        execution_count: executionCount
-      };
+    // function publish_execute_error(result: string, executionCount: number) {
+    //   const bundle = {
+    //     data: {
+    //       'text/plain': result
+    //     },
+    //     metadata: {},
+    //     execution_count: executionCount
+    //   };
 
-      postMessage({
-        bundle,
-        type: 'execute_error'
-      });
+    //   postMessage({
+    //     bundle,
+    //     type: 'execute_error'
+    //   });
 
-      return {
-        status: 'error',
-        user_expressions: {}
-      };
-    }
+    //   return {
+    //     status: 'error',
+    //     user_expressions: {}
+    //   };
+    // }
 
     // function publishMessage(
     //   msg: string,
@@ -153,196 +153,198 @@ export class AIRemoteKernel {
       cell_text: string,
       executionCount: number
     ) {
-      if (MyConsole.inDebug) {
-        return cell_text;
-      }
-      const [actions, pureMessage] = extractPersonAndMessage(cell_text);
+      return cell_text;
 
-      MyConsole.debug('actions:', actions);
-      MyConsole.debug('actions:', pureMessage);
-      if (actions.length > 1) {
-        return publish_execute_error(
-          '@ 2 or more actions are not supported so far!',
-          executionCount
-        ); // We support this feature in the long future.
-      } else if (actions.length === 1) {
-        const theTemplateName = actions[0].substring(1);
+      // if (MyConsole.inDebug) {
+      //   return cell_text;
+      // }
+      // const [actions, pureMessage] = extractPersonAndMessage(cell_text);
 
-        if (!promptTemplate.get_global_templates()[theTemplateName]) {
-          let errorMsg =
-            'The action ' +
-            theTemplateName +
-            ' is not defined! Please check. \n FYI: The current list is as the following:';
+      // MyConsole.debug('actions:', actions);
+      // MyConsole.debug('actions:', pureMessage);
+      // if (actions.length > 1) {
+      //   return publish_execute_error(
+      //     '@ 2 or more actions are not supported so far!',
+      //     executionCount
+      //   ); // We support this feature in the long future.
+      // } else if (actions.length === 1) {
+      //   const theTemplateName = actions[0].substring(1);
 
-          for (const key in promptTemplate.get_global_templates()) {
-            if (promptTemplate.get_global_templates()[key] === undefined) {
-              continue;
-            }
-            errorMsg += '\n' + key;
-          }
-          return publish_execute_error(errorMsg, executionCount);
-        } else {
-          if (pureMessage.trim().length === 0) {
-            promptTemplate
-              .get_global_templates()
-              [theTemplateName].startNewSession();
-            return publish_execute_result(
-              'The chat history with ' +
-                theTemplateName +
-                ' has been cleared. Now you have a new session with it.',
-              executionCount
-            );
-          }
-        }
-      }
+      //   if (!promptTemplate.get_global_templates()[theTemplateName]) {
+      //     let errorMsg =
+      //       'The action ' +
+      //       theTemplateName +
+      //       ' is not defined! Please check. \n FYI: The current list is as the following:';
 
-      if (pureMessage.length * 2 > promptTemplate.MaxTokenLimit) {
-        return publish_execute_error(
-          'The maxinum of input should be half of ' +
-            promptTemplate.MaxTokenLimit,
-          executionCount
-        );
-      }
+      //     for (const key in promptTemplate.get_global_templates()) {
+      //       if (promptTemplate.get_global_templates()[key] === undefined) {
+      //         continue;
+      //       }
+      //       errorMsg += '\n' + key;
+      //     }
+      //     return publish_execute_error(errorMsg, executionCount);
+      //   } else {
+      //     if (pureMessage.trim().length === 0) {
+      //       promptTemplate
+      //         .get_global_templates()
+      //         [theTemplateName].startNewSession();
+      //       return publish_execute_result(
+      //         'The chat history with ' +
+      //           theTemplateName +
+      //           ' has been cleared. Now you have a new session with it.',
+      //         executionCount
+      //       );
+      //     }
+      //   }
+      // }
 
-      let theTemplateName = 'ai';
-      if (actions[0]) {
-        theTemplateName = actions[0].substring(1);
-      }
+      // if (pureMessage.length * 2 > promptTemplate.MaxTokenLimit) {
+      //   return publish_execute_error(
+      //     'The maxinum of input should be half of ' +
+      //       promptTemplate.MaxTokenLimit,
+      //     executionCount
+      //   );
+      // }
 
-      let messages2send: ChatMessage[] = [];
-      let usrContent = '';
-      const statuses: { [key: string]: string } = { cell_text: pureMessage };
+      // let theTemplateName = 'ai';
+      // if (actions[0]) {
+      //   theTemplateName = actions[0].substring(1);
+      // }
 
-      if (actions.length === 0) {
-        //No actions are mentioned
-        messages2send.push({ role: 'user', content: pureMessage });
-      } else {
-        // The mentioned actions, which are critical to the following processing
-        MyConsole.table(actions);
-        const p = promptTemplate
-          .get_global_templates()
-          [theTemplateName].buildMessages2send(statuses);
-        messages2send = messages2send.concat(p.messages2send);
-        usrContent = p.usrContent;
-      }
-      if (messages2send.length === 0) {
-        // if some exception happened, we may give some default but simple processing
-        messages2send.push({ role: 'user', content: usrContent });
-      }
-      MyConsole.table(messages2send);
+      // let messages2send: ChatMessage[] = [];
+      // let usrContent = '';
+      // const statuses: { [key: string]: string } = { cell_text: pureMessage };
 
-      const startTime = performance.now();
+      // if (actions.length === 0) {
+      //   //No actions are mentioned
+      //   messages2send.push({ role: 'user', content: pureMessage });
+      // } else {
+      //   // The mentioned actions, which are critical to the following processing
+      //   MyConsole.table(actions);
+      //   const p = promptTemplate
+      //     .get_global_templates()
+      //     [theTemplateName].buildMessages2send(statuses);
+      //   messages2send = messages2send.concat(p.messages2send);
+      //   usrContent = p.usrContent;
+      // }
+      // if (messages2send.length === 0) {
+      //   // if some exception happened, we may give some default but simple processing
+      //   messages2send.push({ role: 'user', content: usrContent });
+      // }
+      // MyConsole.table(messages2send);
 
-      try {
-        let completion: any = null;
-        if (MyConsole.inDebug) {
-          completion = await OpenAIDriver.get_globalOpenAI().getChatCompletions(
-            'gpt-35-turbo',
-            messages2send
-          );
-        } else {
-          //Todo: 1. To add delay at the 1st fail.
-          //Todo: 2. extend the delay when the code is too old
-          //Todo: 3. log the retry times
-          //Todo: 4. extend delay after too much consumption
-          completion = await backOff(() =>
-            OpenAIDriver.get_globalOpenAI().getChatCompletions(
-              'gpt-35-turbo',
-              messages2send
-            )
-          );
-        }
+      // const startTime = performance.now();
 
-        MyConsole.table('completion.choices', completion.choices);
+      // try {
+      //   let completion: any = null;
+      //   if (MyConsole.inDebug) {
+      //     completion = await OpenAIDriver.get_globalOpenAI().getChatCompletions(
+      //       'gpt-35-turbo',
+      //       messages2send
+      //     );
+      //   } else {
+      //     //Todo: 1. To add delay at the 1st fail.
+      //     //Todo: 2. extend the delay when the code is too old
+      //     //Todo: 3. log the retry times
+      //     //Todo: 4. extend delay after too much consumption
+      //     completion = await backOff(() =>
+      //       OpenAIDriver.get_globalOpenAI().getChatCompletions(
+      //         'gpt-35-turbo',
+      //         messages2send
+      //       )
+      //     );
+      //   }
 
-        const response = completion.choices[0].message?.content ?? '';
-        //Todo: We should check the response carefully
+      //   MyConsole.table('completion.choices', completion.choices);
 
-        let theTemplate = promptTemplate.get_global_templates()['ai'];
+      //   const response = completion.choices[0].message?.content ?? '';
+      //   //Todo: We should check the response carefully
 
-        if (promptTemplate.get_global_templates()[theTemplateName]) {
-          theTemplate = promptTemplate.get_global_templates()[theTemplateName];
-        }
-        //To add the prompt message here
-        theTemplate.addMessage(
-          'user',
-          usrContent,
-          '',
-          completion.usage?.prompt_tokens || 0
-        );
-        //To add the completion message here
-        theTemplate.addMessage(
-          'assistant',
-          response || '',
-          '',
-          completion.usage?.completion_tokens || 0
-        );
-        if (theTemplate.withMemory) {
-          theTemplate.newSession = false;
-        }
+      //   let theTemplate = promptTemplate.get_global_templates()['ai'];
 
-        // To process error in completion
-        const error = completion.choices[0].finishReason;
+      //   if (promptTemplate.get_global_templates()[theTemplateName]) {
+      //     theTemplate = promptTemplate.get_global_templates()[theTemplateName];
+      //   }
+      //   //To add the prompt message here
+      //   theTemplate.addMessage(
+      //     'user',
+      //     usrContent,
+      //     '',
+      //     completion.usage?.prompt_tokens || 0
+      //   );
+      //   //To add the completion message here
+      //   theTemplate.addMessage(
+      //     'assistant',
+      //     response || '',
+      //     '',
+      //     completion.usage?.completion_tokens || 0
+      //   );
+      //   if (theTemplate.withMemory) {
+      //     theTemplate.newSession = false;
+      //   }
 
-        if (error === 'tokenLimitReached') {
-          return publish_execute_error(
-            'The token Limit Reached error happened. You may wait for a few seconds and try again.',
-            executionCount
-          );
-        } else if (error === 'contentFiltered') {
-          return publish_execute_error(
-            'The Content Filtered error happened in your input or the generated response. You may change your input and try again.',
-            executionCount
-          );
-        }
+      //   // To process error in completion
+      //   const error = completion.choices[0].finishReason;
 
-        const md_iconURL = theTemplate.get_Markdown_iconURL();
+      //   if (error === 'tokenLimitReached') {
+      //     return publish_execute_error(
+      //       'The token Limit Reached error happened. You may wait for a few seconds and try again.',
+      //       executionCount
+      //     );
+      //   } else if (error === 'contentFiltered') {
+      //     return publish_execute_error(
+      //       'The Content Filtered error happened in your input or the generated response. You may change your input and try again.',
+      //       executionCount
+      //     );
+      //   }
 
-        const md_displayName = theTemplate.get_Markdown_DisplayName();
+      //   const md_iconURL = theTemplate.get_Markdown_iconURL();
 
-        // debugger();
-        let json_request = '';
+      //   const md_displayName = theTemplate.get_Markdown_DisplayName();
 
-        if (MyConsole.inDebug) {
-          json_request =
-            '**Prompt in JSON:**</p><p>' +
-            '```json\n' +
-            JSON.stringify(messages2send, null, 2) +
-            '\n```';
-        }
+      //   // debugger();
+      //   let json_request = '';
 
-        const endTime = performance.now();
-        const executionTime = endTime - startTime;
+      //   if (MyConsole.inDebug) {
+      //     json_request =
+      //       '**Prompt in JSON:**</p><p>' +
+      //       '```json\n' +
+      //       JSON.stringify(messages2send, null, 2) +
+      //       '\n```';
+      //   }
 
-        let timepassed = '';
-        if (MyConsole.inDebug) {
-          timepassed = '\n(Execution time: ' + executionTime + ' milliseconds)';
-        }
+      //   const endTime = performance.now();
+      //   const executionTime = endTime - startTime;
 
-        return publish_execute_result(
-          json_request +
-            '</p><p>' +
-            '<table><tbody><tr><td align="left"><p><b>' +
-            md_displayName +
-            '</b>' +
-            md_iconURL +
-            '</p></td>' +
-            '<td align="left">' +
-            response || '' + '</td>' + '</tr></tbody></table>' + timepassed,
-          executionCount
-        );
-      } catch (error: any) {
-        return publish_execute_error(
-          '<p>**Error during getChatCompletions**:' +
-            error.message +
-            '</p><p>**Stack trace**:' +
-            error.stack +
-            '</p><p>' +
-            // AIKernel.api_errors +
-            '</p>',
-          executionCount
-        );
-      }
+      //   let timepassed = '';
+      //   if (MyConsole.inDebug) {
+      //     timepassed = '\n(Execution time: ' + executionTime + ' milliseconds)';
+      //   }
+
+      //   return publish_execute_result(
+      //     json_request +
+      //       '</p><p>' +
+      //       '<table><tbody><tr><td align="left"><p><b>' +
+      //       md_displayName +
+      //       '</b>' +
+      //       md_iconURL +
+      //       '</p></td>' +
+      //       '<td align="left">' +
+      //       response || '' + '</td>' + '</tr></tbody></table>' + timepassed,
+      //     executionCount
+      //   );
+      // } catch (error: any) {
+      //   return publish_execute_error(
+      //     '<p>**Error during getChatCompletions**:' +
+      //       error.message +
+      //       '</p><p>**Stack trace**:' +
+      //       error.stack +
+      //       '</p><p>' +
+      //       // AIKernel.api_errors +
+      //       '</p>',
+      //     executionCount
+      //   );
+      // }
     }
 
     const { code } = content;
@@ -383,22 +385,23 @@ export class AIRemoteKernel {
         // result = code;
       }
 
-      const bundle = {
-        data: {
-          'text/plain': result
-        },
-        metadata: {},
-        execution_count: this._executionCount
-      };
-      postMessage({
-        bundle,
-        type: 'execute_result'
-      });
+      return publish_execute_result(result, this._executionCount);
+      // const bundle = {
+      //   data: {
+      //     'text/plain': result
+      //   },
+      //   metadata: {},
+      //   execution_count: this._executionCount
+      // };
+      // postMessage({
+      //   bundle,
+      //   type: 'execute_result'
+      // });
 
-      return {
-        status: 'ok',
-        user_expressions: {}
-      };
+      // return {
+      //   status: 'ok',
+      //   user_expressions: {}
+      // };
     } catch (e) {
       const { name, stack, message } = e as any as Error;
       const bundle = {
